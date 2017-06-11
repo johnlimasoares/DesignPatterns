2f11ba3a3838b0c19d1477c54c8297d3655fd87c
john <johnlimasoares@gmail.com>
2017-06-11 14:25:17 -0300

    Proxy

---
+++ Proxy/ListUnmodified.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Proxy
+{
+    public class ListUnmodified : IList
+    {
+        private System.Collections.Generic.IList<object> list;
+
+        public ListUnmodified(IList<object> list)
+        {
+            this.list = list;
+        }
+
+
+        public object this[int index] { get { return this[index]; } set { throw new Exception("Não é possível executar esta operação em uma lista não modificável"); } }
+
+        public bool IsReadOnly { get; set; }
+
+        public bool IsFixedSize { get; set; }
+
+        public int Count { get { return list.Count; } }
+
+        public object SyncRoot { get; set; }
+
+        public bool IsSynchronized { get; set; }
+
+        public int Add(object value)
+        {
+            throw new Exception("Não é possível executar esta operação em uma lista não modificável");
+        }
+
+        public void Clear()
+        {
+            throw new Exception("Não é possível executar esta operação em uma lista não modificável");
+        }
+
+        public bool Contains(object value)
+        {
+            return this.list.Contains(value);
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new Exception("Não é possível executar esta operação em uma lista não modificável");
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this.list.GetEnumerator();
+        }
+
+        public int IndexOf(object value)
+        {
+            return this.list.IndexOf(value);
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new Exception("Não é possível executar esta operação em uma lista não modificável");
+        }
+
+        public void Remove(object value)
+        {
+            throw new Exception("Não é possível executar esta operação em uma lista não modificável");
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new Exception("Não é possível executar esta operação em uma lista não modificável");
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var listaString = new List<object>();
+                listaString.Add("Ola");
+                listaString.Add("Mundo");
+
+                var listUnmodified = new ListUnmodified(listaString);
+                listUnmodified.Add("Novo");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.Write(ex.Message);
+                Console.ReadKey();
+
+            }
+
+        }
+    }
+}
